224d26ad48cfe9233019320f4dce8fd08b2f7252
newhorizon-tech <alaukikinbox@gmail.com>
2022-01-04 22:23:51 +0530

    Run API fetching on page load using useEffect, useSelector

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 
-function App() {
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+import {trendingAsync} from './redux/trendingSlice';
+
+const App = () => {
+  const dispatch = useDispatch();
+   const projects = useSelector(state => state.trending.projects);
+   const loaded = useSelector(state => state.trending.status === 'loaded');
+
+    useEffect(() => {
+    if (!loaded) {
+      dispatch(trendingAsync());
+    }
+  }, []);
+
   return (
-    <div className="App">
+    <div>
       <h1> Github Trending </h1>
     </div>
   );
