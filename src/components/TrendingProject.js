87f314352b2539044ce3f286cc3301b78bbd3661
newhorizon-tech <alaukikinbox@gmail.com>
2022-01-08 00:09:41 +0530

    Add fetching data funcitonality on project page

--- src/components/TrendingProject.js
+++ src/components/TrendingProject.js
@@ -1,6 +1,33 @@
-const TrendingProject = (props) => {
-  const {project} = props
+import { useParams } from "react-router-dom";
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+import {trendingAsync} from '../redux/trendingSlice';
+import ProjectDetails from './ProjectDetails'
+
+const TrendingProject = () => {
+  const params = useParams();
+  const {projectId} = params;
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.trending.status === 'loading');
+  const loaded = useSelector(state => state.trending.status === 'loaded');
+  const project = useSelector(state => state.trending.projects.find(x => x.rank == projectId));
+
+  useEffect(() => {
+   if (!loaded) {
+     dispatch(trendingAsync());
+    }
+   }, []);
+
+  console.log(loaded)
+  console.log(project)
+
   return (
-    <h1> {project.repositoryName} </h1>
+    <div>
+      {loading ? 'Loading' : ''}
+      {loaded ?  <ProjectDetails project = {project}/> : ''}
+    </div>
   );
 }
+
+
+export default TrendingProject;
